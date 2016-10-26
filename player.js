33d50612f911931f28a9cad99cd8dd51f2152ddf
kasora <kasorasun@gmail.com>
2016-10-26 12:23:58 +0800

    fk the showingFreq
    just read it in height

--- player.js
+++ player.js
@@ -295,12 +295,13 @@ class Player {
         value /= 2.56;
         
         // Only in the drop
-        if(value < this.showingFreq[offset]){
-            let dist = this.showingFreq[offset] - value;
+        
+        let prevValue = bar.style.height.substring(0, bar.style.height.length-1);
+        prevValue = parseFloat(prevValue);
+        if(value < prevValue){
+            let dist = prevValue - value;
             value += dist * (1 - this.dropRate);
-        }  
-
-        this.showingFreq[offset] = value;
+        }        
         bar.style.height = value + '%';
     }
 
@@ -443,7 +444,6 @@ class Player {
             newBar.id = `bar${i}`;
             this.visualNode.appendChild(newBar);
             this.barArray.push(newBar);
-            this.showingFreq[i] = 0;
         }
     }
 }
